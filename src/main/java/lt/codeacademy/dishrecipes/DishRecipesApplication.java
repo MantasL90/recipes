7dba1b64256fc67940eeb4a5b9f8464748d70ff4
MantasM90 <matuzas2020@gmail.com>
2022-07-27 17:14:39 +0300

    init Spring Web app

---
+++ src/main/java/lt/codeacademy/dishrecipes/DishRecipesApplication.java
@@ -0,0 +1,13 @@
+package lt.codeacademy.dishrecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DishRecipesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/codeacademy/dishrecipes/DishRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.codeacademy.dishrecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DishRecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
